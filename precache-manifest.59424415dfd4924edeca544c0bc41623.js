a4a3020f58675a2bdc7c9579ae38fab124a9a9c6
Ayaz-Anis00 <58621127+Ayaz-Anis00@users.noreply.github.com>
2020-07-29 10:43:30 +0530

    Updates

---
+++ precache-manifest.59424415dfd4924edeca544c0bc41623.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e61bed31ab1fa4e85e7015608c0217e",
+    "url": "/lesson_5/index.html"
+  },
+  {
+    "revision": "32a19b3f892e8375c444",
+    "url": "/lesson_5/static/css/main.10610af1.chunk.css"
+  },
+  {
+    "revision": "accba10d890178796615",
+    "url": "/lesson_5/static/js/2.5834e7d9.chunk.js"
+  },
+  {
+    "revision": "32a19b3f892e8375c444",
+    "url": "/lesson_5/static/js/main.51b20db5.chunk.js"
+  },
+  {
+    "revision": "cfa1e6404835af62dd09",
+    "url": "/lesson_5/static/js/runtime~main.1ddc7df3.js"
+  }
+]);
